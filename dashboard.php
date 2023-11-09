10a4ce93f100aad8a32e2573d5ab6b884262c189
OGUNDIRAN VICTOR ISREAL <victoroluwadarasimi16@gmail.com>
2023-11-09 15:13:58 +0100

    file upload commit

--- dashboard.php
+++ dashboard.php
@@ -2,23 +2,17 @@
 require 'connection.php';
 session_start();
 if(isset($_SESSION['userid'])){
-    $id = $_SESSION['userid'];
-    echo $id;
-    
-    $query = "SELECT * FROM students WHERE user_id=$id";
+    $userid = $_SESSION['userid'];
+    echo $userid;
+    $query = "SELECT * FROM students WHERE user_id=$userid";
     $querycon=$dbconnection->query($query);
-    print_r($querycon);
+    // print_r($querycon);
     $user = $querycon->fetch_assoc();
-    print_r($user);
+    // print_r($user);
+    $firstname = $user['first_name'];
+    $picture = $user['profile_pic'];
+    echo $picture;
 
-//     $query = "SELECT first_name FROM students WHERE first_name = '$firstName'";
-//     $result = $dbconnection->query($query);
-//     if ($result->num_rows > 0) {
-//     $row = $result->fetch_assoc();
-//     $firstName = $row['first_name'];
-// } else {
-//     echo "No results found.";
-// }
 };
 ?>
 <!DOCTYPE html>
@@ -29,7 +23,111 @@ if(isset($_SESSION['userid'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>Document</title>
 </head>
+<style>
+    body{
+        background-color: #E0E0E0
+    }
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+.container {
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  background-color: #fcfcfc;
+}
+
+.card {
+  border-radius: 10px;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.3);
+  width: 600px;
+  height: 280px;
+  background-color: #ffffff;
+  padding: 10px 30px 40px;
+}
+
+.card h3 {
+  font-size: 22px;
+  font-weight: 600;
+  
+}
+
+.drop_box {
+  margin: 10px 0;
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border: 3px dotted #a3a3a3;
+  border-radius: 5px;
+}
+
+.drop_box h4 {
+  font-size: 16px;
+  font-weight: 400;
+  color: #2e2e2e;
+}
+
+.drop_box p {
+  margin-top: 10px;
+  margin-bottom: 20px;
+  font-size: 12px;
+  color: #a3a3a3;
+}
+
+.btn:hover{
+  text-decoration: none;
+  background-color: #ffffff;
+  color: #005af0;
+  padding: 10px 20px;
+  border: none;
+  outline: 1px solid #010101;
+}
+.form input {
+  margin: 10px 0;
+  width: 100%;
+  background-color: #e2e2e2;
+  border: none;
+  outline: none;
+  padding: 12px 20px;
+  border-radius: 4px;
+}
+
+    
+</style>
 <body>
-    <h1>Welcome to Dashboard <?php echo $firstName ?></h1>
+    <h1>Welcome to Dashboard </h1>
+    <!-- <p><?php echo $user['first_name'] ?></p> -->
+
+    <div class="container">
+  <div class="card">
+    <h3>Upload Files</h3>
+    <div class="drop_box">
+      <header>
+        <h4>Select File here</h4>
+      </header>
+      <p>Files Supported: PDF, TEXT, DOC , DOCX</p>
+    <form action="fileupload.php" method="post" enctype="multipart/form-data">
+        <label for="file">Choose an image:</label>
+        <input type="file" class="btn" name="profile_pic">
+        <input type="submit" class="btn btn-success" value="Upload profile Pic" name="submit" >
+    </form>
+    </div>
+  </div>
+</div>
+<?php
+echo '<img src="images/' . $profile_pic . '" alt="profile_pic"';
+?>
+
 </body>
 </html>
